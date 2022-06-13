86a7d73a0dc0b18c7a35db7a668908d8f204440a
Kent1C-38 <corsiusquentin@gmail.com>
2022-06-13 15:29:56 +0200

    juste a test

--- src/main/java/fr/kent1c38/test/Test.java
+++ src/main/java/fr/kent1c38/test/Test.java
@@ -1,7 +1,11 @@
 package fr.kent1c38.test;
 
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -10,6 +14,8 @@ import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
 
 @Mod("test")
 public class Test
@@ -37,6 +43,22 @@ public class Test
     }
     private void processIMC(final InterModProcessEvent event)
     {
-        LOGGER.info("Got IMC {}");
+        LOGGER.info("Got IMC {}", event.getIMCStream().map(m->m.messageSupplier().get()).collect(Collectors.toList()));
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        LOGGER.info("HELLO from the server starting");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+        @SubscribeEvent
+        public static void onBlockRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
+        {
+            LOGGER.info("HELLO from register Block");
+        }
     }
 }
