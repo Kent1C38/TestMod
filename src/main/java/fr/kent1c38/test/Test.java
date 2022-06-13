cb73dc2f9f17a86cecd4271b8d9ecf9d68a96adb
Kent1C-38 <corsiusquentin@gmail.com>
2022-06-13 15:22:54 +0200

    Initial commit

---
+++ src/main/java/fr/kent1c38/test/Test.java
@@ -0,0 +1,42 @@
+package fr.kent1c38.test;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("test")
+public class Test
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Test()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        InterModComms.sendTo("exemplemod", "helloworld", () -> {LOGGER.info("Hello world from the MDK"); return "Hello world";});
+    }
+    private void processIMC(final InterModProcessEvent event)
+    {
+        LOGGER.info("Got IMC {}");
+    }
+}
