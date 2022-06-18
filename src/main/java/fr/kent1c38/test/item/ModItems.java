f9b57c03edeb884ce4d8e15e1d0d7ca7d9628d08
Kent1C38 <96208145+Kent1C38@users.noreply.github.com>
2022-06-18 14:41:58 +0200

    Update ModItems.java

--- src/main/java/fr/kent1c38/test/item/ModItems.java
+++ src/main/java/fr/kent1c38/test/item/ModItems.java
@@ -16,6 +16,7 @@ public class ModItems
 
     public static final RegistryObject<Item> RAW_RED_IRON = ITEMS.register("raw_red_iron", () -> new Item(new Item.Properties().tab(ModCreativeModTab.TEST_TAB)));
     public static final RegistryObject<Item> RED_IRON_INGOT = ITEMS.register("red_iron_ingot", () -> new Item(new Item.Properties().tab(ModCreativeModTab.TEST_TAB)));
+    public static final RegistryObject<Item> URANIUM = ITEMS.register("uranium", () -> new Item(new Item.Properties().tab(ModCreativeModTab.TEST_TAB)));
     public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod", () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModTab.TEST_TAB).durability(32)));
 
     //public static final RegistryObject<Item> CONTROLLER = ITEMS.register("controller", ()-> new Item(new Item.Properties().tab(ModCreativeModeTab)));
