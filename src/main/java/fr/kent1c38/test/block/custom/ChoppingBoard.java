6f6d0639cc4cd665dd246f696acfa66b3c73f390
Kent1C-38 <corsiusquentin@gmail.com>
2022-06-20 18:26:39 +0200

    big update

---
+++ src/main/java/fr/kent1c38/test/block/custom/ChoppingBoard.java
@@ -0,0 +1,22 @@
+package fr.kent1c38.test.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class ChoppingBoard extends Block
+{
+    public ChoppingBoard(Properties p_49795_) {
+        super(p_49795_);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+    }
+}
